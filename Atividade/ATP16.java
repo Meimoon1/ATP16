5863dafa7f7628afe39734c90296dfe645d7ad5a
meimoon1 <melevelyn69@gmail.com>
2021-09-30 13:59:33 -0300

    Modificacoes

--- Atividade/ATP16.java
+++ Atividade/ATP16.java
@@ -8,16 +8,17 @@ public class ATP16 {
         CalculadoraTaxas c = new CalculadoraTaxas();
         double t = c.taxaTransferencia(1200.76);
 
-        System.out.println(t);
+        System.out.printf("A taxa de transferência é: %.2f \n",t);
 
+        System.out.println("A cada cinco sanques, será cobrado um valor de R$ 1,30");
         System.out.print("Informe a quantidade de sanques: ");
         int qtdsaques = Integer.parseInt(sc.nextLine());
         System.out.print("Informe o valor transferido: ");
         double valorT = Double.parseDouble(sc.nextLine());
 
         double t2= c.taxaSaque(qtdsaques, valorT);
-        System.out.println("A cada cinco sanques, será cobrado um valor de R$ 1,30");
-        System.out.printf("O valor transferido total é: %f ", t2);
+        
+        System.out.printf("O valor transferido total é: %.2f ", t2);
 
         
 
