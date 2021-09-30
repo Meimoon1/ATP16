18ec5d6b1f70141aac7f5c55f63d580020aad488
meimoon1 <melevelyn69@gmail.com>
2021-09-30 13:55:49 -0300

    Chamando segundo método

--- Atividade/ATP16.java
+++ Atividade/ATP16.java
@@ -1,11 +1,25 @@
 package Atividade;
+import java.util.Scanner;
 
 public class ATP16 {
    public static void main(String[] args) {
+
+       Scanner sc = new Scanner(System.in);
         CalculadoraTaxas c = new CalculadoraTaxas();
-        double t = c.calcularTransferencia(1200.76);
+        double t = c.taxaTransferencia(1200.76);
 
         System.out.println(t);
+
+        System.out.print("Informe a quantidade de sanques: ");
+        int qtdsaques = Integer.parseInt(sc.nextLine());
+        System.out.print("Informe o valor transferido: ");
+        double valorT = Double.parseDouble(sc.nextLine());
+
+        double t2= c.taxaSaque(qtdsaques, valorT);
+        System.out.println("A cada cinco sanques, será cobrado um valor de R$ 1,30");
+        System.out.printf("O valor transferido total é: %f ", t2);
+
+        
 
         
    }
