df662a964987214107e344b7c700b56bd8d08de6
meimoon1 <melevelyn69@gmail.com>
2021-09-30 16:27:54 -0300

    Implementações

--- Atividade/CalculadoraTaxas.java
+++ Atividade/CalculadoraTaxas.java
@@ -1,30 +1,25 @@
 package Atividade;
 
 public class CalculadoraTaxas {
-    int contSaque = 0;
-    double taxa = 0;
+    private int contSaque = 0;
+    private double taxa = 0;
+    private double taxaTransferencia = 0.001;
+    private double taxaSaque = 1.30;
 
     public double taxaTransferencia(double valorTransferido){
-        double valorRetornado = valorTransferido* 0.001;
+        double valorRetornado = valorTransferido* taxaTransferencia;
         return valorRetornado; // taxas sobre transferencia
     }
 
-    
     public double taxaSaque(){
         contSaque +=1;
         
         if(contSaque%5 ==0){
-            taxa = taxa + 1.30;
+            taxa = taxa + taxaSaque;
             return taxa;
         }else{
             return 0;
         }
       
-    } 
-
-    
-   
-
-
-    
+    }    
 }
