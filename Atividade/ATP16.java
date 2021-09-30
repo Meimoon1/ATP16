355cf6abe4a8cabb455741780be87c0ffe669f72
meimoon1 <melevelyn69@gmail.com>
2021-09-30 12:18:02 -0300

    Criando objeto e chamado metodo transferencia

--- Atividade/ATP16.java
+++ Atividade/ATP16.java
@@ -1,5 +1,13 @@
 package Atividade;
 
 public class ATP16 {
+   public static void main(String[] args) {
+        CalculadoraTaxas c = new CalculadoraTaxas();
+        double t = c.calcularTransferencia(1200.76);
+
+        System.out.println(t);
+
+        
+   }
     
 }
--- Atividade/CalculadoraTaxas.java
+++ Atividade/CalculadoraTaxas.java
@@ -1,5 +1,12 @@
 package Atividade;
 
 public class CalculadoraTaxas {
-    
+     double valorRetornado;
+
+    public double calcularTransferencia(double valorTransferido){
+        double valorRetornado = valorTransferido + valorTransferido* 0.001;
+        return valorRetornado;
+    }
+
+    //public void calcularSaques(double )
 }
