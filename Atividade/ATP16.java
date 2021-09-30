e555d9fddec505ef9e219a00ef1befe1037c82ca
meimoon1 <melevelyn69@gmail.com>
2021-09-30 14:36:35 -0300

    Consertando o problema

--- Atividade/ATP16.java
+++ Atividade/ATP16.java
@@ -6,22 +6,22 @@ public class ATP16 {
 
        Scanner sc = new Scanner(System.in);
         CalculadoraTaxas c = new CalculadoraTaxas();
-        System.out.println("Será acrescentado uma taxa de 0.001% por transação");
+        
         System.out.println("Informe o valor da transferência: ");
         double transferencia = Double.parseDouble(sc.nextLine());
         double t = c.taxaTransferencia(transferencia);
 
-        System.out.printf("O valor total de transferência é: %.2f \n",t);
+        System.out.printf("A taxa da transferência foi: R$ %.2f \n",t);
 
-        System.out.println("A cada cinco sanques, será cobrado um valor de R$ 1,30");
+       /* System.out.println("*----------A cada cinco sanques, será cobrado um valor de R$ 1,30------------*");
         System.out.print("Informe a quantidade de sanques: ");
         int qtdsaques = Integer.parseInt(sc.nextLine());
         System.out.print("Informe o valor transferido: ");
         double valorT = Double.parseDouble(sc.nextLine());
 
-        double t2= c.taxaSaque(qtdsaques, valorT);
+       double t2= c.taxaSaque(qtdsaques, valorT); */
         
-        System.out.printf("O valor transferido total é: %.2f ", t2);
+       // System.out.printf("O valor transferido total é: %.2f ", t2);
 
         
 
