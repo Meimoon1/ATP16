b24ad6579a43ab786e7f93dbac8b3ac2fff36bc5
meimoon1 <melevelyn69@gmail.com>
2021-10-01 15:16:09 -0300

    Implementação da impressão dos resultados

--- ATP17/Principal.java
+++ ATP17/Principal.java
@@ -2,16 +2,16 @@ package ATP17;
 
 public class Principal {
     public static void main(String[] args) {
-        Calculo c = new Calculo();
+        Calculo c = new Calculo(); 
 
        double r = c.calcularIss(1500);
-       System.out.println(r);
-       double r1 = c.calcularIof(4000);
-       System.out.println(r1);
-       double r2 = c.calcularIr(2000);
-       System.out.println(r2);
-        
+       System.out.printf("O imposto ISS do valor recebido é: %.2f \n", r);
 
+       double r1 = c.calcularIof(4000);
+       System.out.printf("O imposto IOF do valor recebido é: %.2f \n",r1);
+
+       double r2 = c.calcularIr(2000);
+       System.out.printf("O imposto IR do valor recebido é: %.2f \n", r2);       
     }
     
 }
