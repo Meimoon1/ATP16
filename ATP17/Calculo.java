b3c705a3596adef58aa418893e294ee81af7f2a6
meimoon1 <melevelyn69@gmail.com>
2021-10-01 15:03:18 -0300

    Comentarios

--- ATP17/Calculo.java
+++ ATP17/Calculo.java
@@ -1,22 +1,26 @@
 package ATP17;
 
 public class Calculo {
-    private double taxaIss = 0.005;
+    //Taxas de cada imposto
+    private double taxaIss = 0.005; 
     private double taxaIof = 0.002;
     private double taxaIr = 0.008;
 
+    //Método criado para calculo do imposto ISS
     public double calcularIss(double valorRecebido){
-        double imposto = valorRecebido * taxaIss;
-        return imposto;
-    }
-    
-    public double calcularIof(double valorRecebido){
-        double imposto = valorRecebido * taxaIof;
-        return imposto;    
+        double impostoCalculado = valorRecebido * taxaIss;
+        return impostoCalculado;
     }
 
+    //Método criado para calculo do imposto IOF
+    public double calcularIof(double valorRecebido){
+        double impostoCalculado = valorRecebido * taxaIof;
+        return impostoCalculado;    
+    }
+
+    //Método criado para calculo do imposto IR
     public double calcularIr(double valorRecebido){
-        double imposto = valorRecebido * taxaIr;
-        return imposto;
+        double impostoCalculado = valorRecebido * taxaIr;
+        return impostoCalculado;
     }
 }
