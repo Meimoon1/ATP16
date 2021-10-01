af62799121542a0c4731b84c909834e20d632d8a
meimoon1 <melevelyn69@gmail.com>
2021-10-01 14:59:51 -0300

    Metodo calcular iof

--- ATP17/Calculo.java
+++ ATP17/Calculo.java
@@ -2,11 +2,18 @@ package ATP17;
 
 public class Calculo {
     private double taxaIss = 0.005;
+    private double taxaIof = 0.002;
+    private double taxaIr = 0.008;
 
     public double calcularIss(double valorRecebido){
         double imposto = valorRecebido * taxaIss;
         return imposto;
     }
     
-    
+    public double calcularIof(double valorRecebido){
+        double imposto = valorRecebido * taxaIof;
+        return imposto;    
+    }
+
+   
 }
