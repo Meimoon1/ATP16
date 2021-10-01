cec9eb3b91d791f689e656489ce9aff3644d8758
meimoon1 <melevelyn69@gmail.com>
2021-10-01 15:08:36 -0300

    Testando classeISS

--- ATP17/Principal.java
+++ ATP17/Principal.java
@@ -3,8 +3,11 @@ package ATP17;
 public class Principal {
     public static void main(String[] args) {
         Calculo c = new Calculo();
+
+       double r = c.calcularIss(1500);
+       System.out.println(r);
         
-        
+
     }
     
 }
