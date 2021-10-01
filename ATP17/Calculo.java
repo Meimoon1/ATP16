e0e00c091419ba7daf900c3346b021189e9fe10b
meimoon1 <melevelyn69@gmail.com>
2021-10-01 14:55:34 -0300

    Criacao metodo calcularIss

--- ATP17/Calculo.java
+++ ATP17/Calculo.java
@@ -1,5 +1,12 @@
 package ATP17;
 
 public class Calculo {
+    private double taxaIss = 0.005;
+
+    public double calcularIss(double valorRecebido){
+        double imposto = valorRecebido * taxaIss;
+        return imposto;
+    }
+    
     
 }
