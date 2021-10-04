6ffdfa4a8aa2641f6f8fc47da8caa12155e3bcb7
meimoon1 <melevelyn69@gmail.com>
2021-10-04 14:33:47 -0300

    Impressão produto

--- ATP18/TelaPrincipalLoja.java
+++ ATP18/TelaPrincipalLoja.java
@@ -1,10 +1,25 @@
 public class TelaPrincipalLoja{
 public static void main(String[] args) {
     Produto p = new Produto();
+
     p.setCodigo(1330);
+    int c = p.getCodigo();
+    System.out.println(c);
+
     p.setNome("Liquidificador");
+    String l = p.getNome();
+    System.out.println(l);
+    
     p.setdescricao("Cor preta, com 5 velocidades");
+    String d = p.getDescicao();
+
     p.setValor(120.90);
+    String v = p.getValor();
+    System.out.println(v);
+
+    
+
+
 }
 
 
