5e2aadce3842e53441a15cf6757f6f0b98bf222b
meimoon1 <melevelyn69@gmail.com>
2021-10-04 17:23:40 -0300

    Atribuindo valores pra cada posicao e imprimindo

--- ATP19/Numeros.java
+++ ATP19/Numeros.java
@@ -1,5 +1,21 @@
 public class Numeros {
     public static void main(String[] args) {
-        
+        //criação array numeros
+        int[] numeros = new int[10];
+        numeros[0] = 12;
+        numeros[1] = 11;
+        numeros[2] = 10;
+        numeros[3] = 45;
+        numeros[4] = 34;
+        numeros[5] = 98;
+        numeros[6] = 23;
+        numeros[7] = 56;
+        numeros[8] = 80;
+        numeros[9] = 24;
+
+        for(int i = 0; i < numeros.length; i++){
+            System.out.println(numeros[i]);
+        }
+
     }
 }
