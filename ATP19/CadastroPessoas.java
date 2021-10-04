ad840dfe87b659e02aa328b0d7b7157cb0235c00
meimoon1 <melevelyn69@gmail.com>
2021-10-04 17:55:48 -0300

    Alteracoes e criacao de objetos na classe cadastro

---
+++ ATP19/CadastroPessoas.java
@@ -0,0 +1,47 @@
+public class CadastroPessoas {
+    public static void main(String[] args) {
+        PessoaFisica[] p = new PessoaFisica[5];
+
+        PessoaFisica p1 = new PessoaFisica();
+        p1.setCodigo(123);
+        p1.setNome("Melissa");
+        p1.setSobrenome("Matos");
+        p1.setIdade(23);
+        p1.setRg("09998393999209");
+        p1.setCpf("009039309303039");
+
+        PessoaFisica p2 = new PessoaFisica();
+        p2.setCodigo(124);
+        p2.setNome("Laercio");
+        p2.setSobrenome("Rios");
+        p2.setIdade(23);
+        p2.setRg("09998393999209");
+        p2.setCpf("009039309303039");
+
+        PessoaFisica p3 = new PessoaFisica();
+        p3.setCodigo(125);
+        p3.setNome("Harry");
+        p3.setSobrenome("Potter");
+        p3.setIdade(15);
+        p3.setRg("09998393999209");
+        p3.setCpf("009039309303039");
+
+        PessoaFisica p4 = new PessoaFisica();
+        p1.setCodigo(126);
+        p1.setNome("Deanerys");
+        p1.setSobrenome("Targeryan");
+        p1.setIdade(22);
+        p1.setRg("09998393999209");
+        p1.setCpf("009039309303039");
+
+        PessoaFisica p5 = new PessoaFisica();
+        p1.setCodigo(123);
+        p1.setNome("Bilbo");
+        p1.setSobrenome("Bolseiro");
+        p1.setIdade(160);
+        p1.setRg("09998393999209");
+        p1.setCpf("009039309303039");
+
+    }
+    
+}
