1fbb77a70db0ec3baf77e476d67401eaca14f5a9
meimoon1 <melevelyn69@gmail.com>
2021-10-04 14:36:45 -0300

    implementando impressao

--- ATP18/TelaPrincipalLoja.java
+++ ATP18/TelaPrincipalLoja.java
@@ -4,22 +4,19 @@ public static void main(String[] args) {
 
     p.setCodigo(1330);
     int c = p.getCodigo();
-    System.out.println(c);
+    System.out.println("Código: "+c);
 
     p.setNome("Liquidificador");
     String l = p.getNome();
-    System.out.println(l);
+    System.out.println("Nome: "+l);
     
     p.setdescricao("Cor preta, com 5 velocidades");
     String d = p.getDescicao();
+    System.out.println("Descricão: "+d);
 
     p.setValor(120.90);
-    String v = p.getValor();
-    System.out.println(v);
-
-    
-
-
+    double v = p.getValor();
+    System.out.println("Valor: "+v);
 }
 
 
