61f1cec8490d305ca57cfcd9384cc7192cbcda38
meimoon1 <melevelyn69@gmail.com>
2021-10-04 17:37:01 -0300

    set e get nome, codigo, idade

---
+++ ATP19/PessoaFisica.java
@@ -0,0 +1,37 @@
+public class PessoaFisica {
+    private int codigo;
+    private String nome;
+    private String sobrenome;
+    private int idade;
+    private String rg;
+    private String cpf;
+
+    public void setCodigo(int codigo){
+        this.codigo = codigo;
+    }
+    public int getCodigo(){
+        return this.codigo;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setSobrenome(String sobrenome){
+        this.sobrenome = sobrenome;
+    }
+    public String getSobrenome(){
+        return this.sobrenome;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    public int getIdade(){
+        return this.idade;
+    }
+
+}
