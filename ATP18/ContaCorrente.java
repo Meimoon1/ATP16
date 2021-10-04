322d1d2270f24ff508562cd517d90249429c5626
meimoon1 <melevelyn69@gmail.com>
2021-10-04 14:18:30 -0300

    set e get numero e codigo

--- ATP18/ContaCorrente.java
+++ ATP18/ContaCorrente.java
@@ -1,9 +1,34 @@
 public class ContaCorrente {
-   private String Agencia;
-   private String Numero;
+   private String agencia;
+   private String numero;
    private int codigo;
    private double saldo;
 
-   
+    public void setAgencia(String agencia){
+        this.agencia = agencia;
+    }
+    public int getAgencia(){
+        return this.agencia;
+    }
+
+    public void setNumero(String numero){
+        this.numero = numero;
+    }
+    public int getNumero(){
+        return this.numero;
+    }
+
+    public void setCodigo(int codigo){
+        this.codigo = codigo;
+    }
+    public int getCodigo(){
+        return this.codigo;
+    }
+
+    
+
+
+
+
 
 }
