9e522a5d1e1ed9466ec5ea83019ae958bc1a10fd
meimoon1 <melevelyn69@gmail.com>
2021-10-04 14:27:54 -0300

    Criando classe tela principal loja e objeto

---
+++ ATP18/TelaPrincipalLoja.java
@@ -0,0 +1,19 @@
+public class TelaPrincipalLoja{
+public static void main(String[] args) {
+    Produto p = new Produto();
+    p.setCodigo(1330);
+    p.setNome("Liquidificador");
+    p.setdescricao("Cor preta, com 5 velocidades");
+    p.setValor(120.90);
+}
+
+
+
+
+
+
+
+
+
+
+}
