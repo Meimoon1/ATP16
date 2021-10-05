9f41889de216a1c3783006e37a37c828f7feb4d6
meimoon1 <melevelyn69@gmail.com>
2021-10-05 15:40:06 -0300

    get e set classe aluno e modificacoes em aluno

--- ATP20/Aluno.java
+++ ATP20/Aluno.java
@@ -1,4 +1,18 @@
 public class Aluno extends Pessoa{
     private String curso;
     private String turma;
+
+    public void setCurso(String curso){
+        this.curso = curso;
+    }
+    public String getCurso(){
+        return curso;
+    }
+
+    public void setTurma(String turma){
+        this.turma = turma;
+    }
+    public String getTurma(){
+        return turma;
+    }
 }
--- ATP20/Coordenador.java
+++ ATP20/Coordenador.java
@@ -2,17 +2,17 @@ public class Coordenador extends Funcionario{
     private String curso_coordenado;
     private double bonus;  
     
-    public void setCurso(String curso_coordenado){
+    public void setCurso_coordenado(String curso_coordenado){
         this.curso_coordenado = curso_coordenado;
     }
-    public String getCurso(){
+    public String getCurso_coordenado(){
         return curso_coordenado;
     }
 
     public void setBonus(double bonus){
         this.bonus = bonus;
     }
-    public String getBonus(){
+    public double getBonus(){
         return bonus;
     }
 }
