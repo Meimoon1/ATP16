c30477c4922fc67e271ffc8c4bc38b7dbfa03bb0
meimoon1 <melevelyn69@gmail.com>
2021-10-05 15:35:01 -0300

    set e get classe funcionario

--- ATP20/Funcionario.java
+++ ATP20/Funcionario.java
@@ -1,4 +1,18 @@
 public class Funcionario extends Pessoa{
     private double salario;
     private String departamento;
+
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+    public double getSalario(){
+        return salario;
+    }
+
+    public void setDepartamento(String departamento){
+        this.departamento = departamento;
+    }
+    public String getDepartamento(){
+        return departamento;
+    }
 }
