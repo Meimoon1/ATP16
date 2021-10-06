866bef5673787813745735fd82127e41dd3d5fcd
meimoon1 <melevelyn69@gmail.com>
2021-10-05 22:38:18 -0300

    criacao objetos

--- ATP20/Universidade.java
+++ ATP20/Universidade.java
@@ -4,7 +4,27 @@ public class Universidade {
         Coordenador c = new Coordenador();
         Aluno a = new Aluno();
 
-        a.set
+        //criando objeto funcionario
+        f.setNome("Maurício");
+        f.setSobrenome("Martins");
+        f.setMatricula("65777380");
+        f.setSalario(1100.56);
+        f.setDepartamento("Corpo doscente");
+
+        //Criando objeto Coordenador
+        c.setNome("Luiz");
+        c.setSobrenome("Souza");
+        c.setMatricula("998727290");
+        c.setCurso_coordenado("História da Arte");
+        c.setBonus(379.00);
+
+        //Criando objeto aluno
+        a.setNome("Luna");
+        a.setSobrenome("Montenegro");
+        a.setMatricula("88700929");
+        a.setCurso("Física");
+        a.setTurma("B");
+        
 
     }
 }
