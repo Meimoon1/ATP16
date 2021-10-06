56a732fcbde3c8c0c3f6a77d6542bc74761a7812
meimoon1 <melevelyn69@gmail.com>
2021-10-06 14:44:07 -0300

    modificando contacorrente

--- ATP21/ContaCorrente.java
+++ ATP21/ContaCorrente.java
@@ -3,10 +3,16 @@ public class ContaCorrente extends Conta {
     private double taxa_manutencao = 2.50;
     private int numero_transferencias = 0;
 
-   /* @Override
-    public void transferencia(double valorTransferencia){
-        super.transferencia(valorTransferencia);
-        double novoSaldo = this.getSaldo() - taxa_transferencia;
-        this.setSaldo(novoSaldo);
-    } */
+    @Override
+    public void transferencia(double valor){  
+        do{
+            super.transferencia(valor);
+            double novoSaldo = this.getSaldo()- taxa_transferencia;
+            this.setSaldo(novoSaldo); 
+            numero_transferencias++;
+                if(numero_transferencias %5 ==0){
+                    novoSaldo = novoSaldo - taxa_manutencao;
+                }
+        }while(numero_transferencias > 0);
+    } 
 }
