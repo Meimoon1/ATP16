81c04eafb00c75b75dadf6d4524de23dfcc491b5
meimoon1 <melevelyn69@gmail.com>
2021-10-06 13:16:54 -0300

    Criacao classe conta e get e sets

---
+++ ATP21/Conta.java
@@ -0,0 +1,18 @@
+public class Conta{
+    private double saldo;
+    private int codigo_cliente;
+
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public void setCodigo(int codigo_cliente){
+        this.codigo_cliente = codigo_cliente;
+    }
+    public int getCodigo(){
+        return codigo_cliente;
+    }
+}
