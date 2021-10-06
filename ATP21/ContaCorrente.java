46199489e9652b2241e6fb99f90dcc58b95e2a53
meimoon1 <melevelyn69@gmail.com>
2021-10-06 13:22:10 -0300

    Conta corrente herdando conta

--- ATP21/ContaCorrente.java
+++ ATP21/ContaCorrente.java
@@ -1,5 +1,5 @@
-public class ContaCorrente {
-    private double taxa_transferencia;
-    private double taxa_manutencao;
-    private double numero_transferencias;
+public class ContaCorrente extends Conta {
+    private double taxa_transferencia = 10.50;
+    private double taxa_manutencao = 2.50;
+    private int numero_transferencias = 0;
 }
