faf8a910978d2c071e939ce9365beb6fc425381c
meimoon1 <melevelyn69@gmail.com>
2021-10-07 17:26:06 -0300

    impressao somas

--- ATP22/TelaCalculadora.java
+++ ATP22/TelaCalculadora.java
@@ -4,9 +4,13 @@ public class TelaCalculadora {
 
        double r =  c.Soma(12.34, 34.12);
        int r1 = c.Soma(2, 4);
-       float  r2 = c.Soma(1.2222222, 3.9909090, 8.909);
-       long  r3 = c.Soma(89E23, 99E12,890E12);
-       
+       float  r2 = c.Soma(10.6f, 38.67f, 89.56f);
+       long  r3 = c.Soma(99999999, 99999999,99999999);
+
+       System.out.println(r);
+       System.out.println(r1);
+       System.out.println(r2);
+       System.out.println(r3);
     }
     
 }
