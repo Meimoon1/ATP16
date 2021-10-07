d041bba92f79e59745a5cba5d0fb1ffd5bd1d5ae
meimoon1 <melevelyn69@gmail.com>
2021-10-07 15:55:34 -0300

    Criacao classe aluno com atributo e get e set nome

---
+++ ATP22/Aluno.java
@@ -0,0 +1,12 @@
+public class Aluno{
+    private String nomeCompleto;
+    private int idade;
+    private String matricula;
+
+    public void setNomeCompleto(String nomeCompleto){
+        this.nomeCompleto = nomeCompleto;
+    }
+    public String getNomeCompleto(){
+        return nomeCompleto;
+    }
+}
