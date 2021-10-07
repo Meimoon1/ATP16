b23b642a57d56c785791017e5d0bc1e5e8ff3fc5
meimoon1 <melevelyn69@gmail.com>
2021-10-07 17:20:26 -0300

    calculadora

---
+++ ATP22/Calculadora.java
@@ -0,0 +1,21 @@
+public class Calculadora {
+    public int Soma(int n1, int n2){
+        int resultado = n1+n2;
+        return resultado;
+    }
+
+    public double Soma(double n1, double n2){
+        double resultado = n1+n2;
+        return resultado;
+    }
+
+    public float Soma(float n1, float n2, float n3){
+        float resultado = n1+ n2 + n3;
+        return resultado;
+    }
+
+    public long Soma(long n1, long n2, long n3){
+        long resultado = n1 + n2 + n3;
+        return resultado;
+    }
+}
