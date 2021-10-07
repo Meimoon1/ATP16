2edab7067943d31c1996ed64cc41a28d5c932538
meimoon1 <melevelyn69@gmail.com>
2021-10-07 16:23:51 -0300

    metodo saque e deposito

--- ATP22/ContaCorrente.java
+++ ATP22/ContaCorrente.java
@@ -1,8 +1,20 @@
 public class ContaCorrente {
     private double saldo;
-    private double taxaSaque;
+    private double taxaSaque = 7.50;
     private int codigoCliente;
 
-    
+    public ContaCorrente(double saldo, double taxaSaque, int codigoCliente){
+        
+    }
+
+    public double saque(double valor){
+        double valorFinal = this.saldo - valor  - taxaSaque;
+        return valorFinal;
+    }
+
+    public double deposito(double valor){
+        double valorFinal = this.saldo + valor;
+        return valorFinal;
+    }
     
 }
